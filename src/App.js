15bfe58a78750de1cbdb47335e5242f9efb98310
Pavlos Nicolaou <pavlos@blind.org.uk>
2018-05-15 13:29:51 +0100

    created the title of the app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello from scratch
+        <table style={{
+          backgroundColor: '#000',
+          display: 'block',
+          color: '#fff',
+          paddingLeft: 16
+        }}>
+          <tbody>
+            <tr>
+              <td>
+              <img width="50" src= "green_app_icon.svg"/>
+              </td>
+              <td width="8" />
+              <td>
+                <h1>Movies DB Search</h1>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
