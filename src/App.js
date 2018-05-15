117c732d12abb8f2909eb4d41a2e8fbfdfa53c0a
Pavlos Nicolaou <pavlos@blind.org.uk>
2018-05-15 14:35:19 +0100

    using ajax to parse data

--- src/App.js
+++ src/App.js
@@ -1,40 +1,67 @@
 import React, { Component } from 'react';
 import './App.css';
+import MovieRow from './MovieRow.js'
+import $ from 'jquery'
 
 class App extends Component {
 
   constructor(props) {
     super(props)
-    console.log("this is my initialiser")
+    this.state = {}
+    // console.log("this is my initialiser")
 
-    const movies = [
-      {id: 0, poster_src: "https://image.tmdb.org/t/p/w185/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
-       title: "Avengers: Infinity War", overview: "As the Avengers and their allies have continued to protect the world from threats too large"},
-      {id: 1, poster_src: "https://image.tmdb.org/t/p/w185/cezWGskPY5x7GaglTTRN4Fugfb8.jpg",
-       title: "	The Avengers", overview: "This is my second overview"},
-    ]
+    // const movies = [
+    //   {id: 0, poster_src: "https://image.tmdb.org/t/p/w185/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+    //    title: "Avengers: Infinity War", overview: "As the Avengers and their allies have continued to protect the world from threats too large"},
+    //   {id: 1, poster_src: "https://image.tmdb.org/t/p/w185/cezWGskPY5x7GaglTTRN4Fugfb8.jpg",
+    //    title: "	The Avengers", overview: "This is my second overview"},
+    // ]
     
-    var movieRows = []
-    movies.forEach((movie) => {
-      console.log(movie.id)
+    // var movieRows = []
+    // movies.forEach((movie) => {
+    //   console.log(movie.id)
+    //   const movieRow = <MovieRow movie={movie} />
+    //   movieRows.push(movieRow)
+    // })
 
-      const movieRow = <table key={movie.id}>
-        <tbody>
-          <tr>
-            <td>
-              <img alt="poster" width="120" src={movie.poster_src}/>
-            </td>
-            <td>
-              {movie.title}
-              <p>{movie.overview}</p>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      movieRows.push(movieRow)
+    // this.state = {rows: movieRows}
+
+    this.performSearch("ant man")
+  }
+
+  performSearch(searchTerm) {
+    console.log("Perform search using moviedb")
+    const urlString = "https://api.themoviedb.org/3/search/movie?api_key=1b5adf76a72a13bad99b8fc0c68cb085&query=" + searchTerm
+    $.ajax({
+      url: urlString,
+      success: (searchResults) => {
+        console.log("Fetched data successfully")
+        // console.log(searchResults)
+        const results = searchResults.results
+        // console.log(results[0])
+
+        var movieRows = []
+
+        results.forEach((movie) => {
+          movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path
+          // console.log(movie.poster_path)
+          const movieRow = <MovieRow key={movie.id} movie={movie}/>
+          movieRows.push(movieRow)
+        })
+
+        this.setState({rows: movieRows})
+      },
+      error: (xhr, status, err) => {
+        console.error("Failed to fetch data")
+      }
     })
+  }
 
-    this.state = {rows: movieRows}
+  searchChangeHandler(event) {
+    console.log(event.target.value)
+    const boundObject = this
+    const searchTerm = event.target.value
+    boundObject.performSearch(searchTerm)
   }
 
   render() {
